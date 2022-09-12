8d267aa573e8eafcd124cbf7993d3c0481984cc1
steveYeah <steve.hutchins@gousto.co.uk>
2022-09-12 23:19:41 +0100

    Create package with basic curry decorator

---
+++ src/pyfuncy/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.1.0'
+
+from pyfuncy.curry import curry
+
+__all__ = ["curry"]
---
+++ src/pyfuncy/curry.py
@@ -0,0 +1,36 @@
+from functools import partial, wraps
+from inspect import signature
+
+
+def curry(fn, *args):
+    """
+    Decorator for currying functions
+    """
+    @wraps(fn)
+    def curried(*args):
+        if len(args) + _get_num_args(fn) == _get_num_fn_params(fn):
+            return fn(*args)
+
+        return curry(partial(fn, *args))
+
+    return curried
+
+def _get_num_args(fn):
+    """
+    Get the number of arguments already applied to the given function
+    """
+    if not isinstance(fn, partial):
+        return 0
+
+    return len(fn.args) + _get_num_args(fn.func)
+
+
+def _get_num_fn_params(fn):
+    """
+    Get the number of parameters required by the given function
+    """
+    if isinstance(fn, partial):
+        return _get_num_fn_params(fn.func)
+
+    return len(signature(fn).parameters)
+
